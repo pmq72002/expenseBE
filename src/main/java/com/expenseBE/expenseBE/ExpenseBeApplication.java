5b806b148f8949e48df7336495badc8fc25dca27
quanpm <quanpm@softdreams.vn>
2026-04-06 11:40:19 +0700

    first init

---
+++ src/main/java/com/expenseBE/expenseBE/ExpenseBeApplication.java
@@ -0,0 +1,13 @@
+package com.expenseBE.expenseBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expenseBE/expenseBE/ExpenseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenseBE.expenseBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
